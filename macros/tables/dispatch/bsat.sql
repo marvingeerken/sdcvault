54d72f32dd29951d581e183a4cefbb8a1632fad6
Marvin Geerken <134588432+marvingeerken@users.noreply.github.com>
2023-11-07 13:30:31 +0100

    Update bsat.sql

--- macros/tables/dispatch/bsat.sql
+++ macros/tables/dispatch/bsat.sql
@@ -1,7 +1,7 @@
-{%- macro bsat(hash_key, hash_key_ma, bhub, sat, ldts='last_updated') -%}
+{%- macro bsat(hash_key, hash_key_ma, bv_parent, sat, ldts='last_updated') -%}
 
     {{ sdcvault.prepend_generated_by() }}
 
-    {{ adapter.dispatch('bsat', 'sdcvault')(hash_key=hash_key, hash_key_ma=hash_key_ma, bhub=bhub, sat=sat, ldts=ldts) -}}
+    {{ adapter.dispatch('bsat', 'sdcvault')(hash_key=hash_key, hash_key_ma=hash_key_ma, bv_parent=bv_parent, sat=sat, ldts=ldts) -}}
 
 {%- endmacro -%}
