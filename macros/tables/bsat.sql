1b376795cb44a617691b69db7d79aeaa8a67e7a1
Marvin Geerken <134588432+marvingeerken@users.noreply.github.com>
2023-11-07 12:55:20 +0100

    Update bsat.sql

--- macros/tables/bsat.sql
+++ macros/tables/bsat.sql
@@ -1,12 +1,12 @@
 {#- This macro creates Business Satellites. -#}
 
-{% macro default__bsat(hash_key, hash_key_ma, bhub, sat, ldts) -%}
+{% macro default__bsat(hash_key, hash_key_ma, bv_parent, sat, ldts) -%}
 
 with 
 
-bhub as (
+bv_parent as (
     select * exclude (last_updated, dv_source)
-    from {{ ref(bhub) }}
+    from {{ ref(bv_parent) }}
 ),
 
 sat as (
@@ -16,13 +16,13 @@ sat as (
 )
 
 select 
-  bhub.*,
+  bv_parent.*,
   {{ dbt_utils.star(ref(sat), except=[hash_key,'hd_'~sat,'last_updated','dv_source'], relation_alias='sat')}},
   sat.last_updated,
   sat.dv_source
-from bhub
+from bv_parent
 inner join sat
-    on bhub.{{ hash_key }} = sat.{{ hash_key }}
+    on bv_parent.{{ hash_key }} = sat.{{ hash_key }}
 where sat.{{ hash_key }} != {{var('sdcvault.ghost_hk')}}::binary(16)
 {% if hash_key_ma %} and not sat.is_deleted {% endif %}
 
