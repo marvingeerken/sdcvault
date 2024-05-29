0ef7ab34c2efef4791f849d157027bb11f0ff470
Marvin Geerken <134588432+marvingeerken@users.noreply.github.com>
2024-05-29 10:36:57 +0000

    esat linting

--- macros/tables/esat.sql
+++ macros/tables/esat.sql
@@ -1,11 +1,11 @@
 {%- macro default__esat(source_models, parent_hash_key, src_ldts, src_rsrc, high_water_mark_bool, table_sample_prob) -%}
 
 {%- set end_of_time = var('sdcvault.end_of_time') -%}
-{%- set src_ldts = datavault4dbt.replace_standard(src_ldts, 'sdcvault.ldts_alias', 'last_updated') -%}
-{%- set src_rsrc = datavault4dbt.replace_standard(src_rsrc, 'sdcvault.rsrc_alias', 'dv_source') -%}
-{%- set high_water_mark_bool = datavault4dbt.replace_standard(high_water_mark_bool, 'sdcvault.high_water_mark_bool', true) -%}
-{%- set limit_sources_num = datavault4dbt.replace_standard(limit_sources_num, 'sdcvault.limit_sources_num', -1) -%}
-{%- set table_sample_prob = datavault4dbt.replace_standard(table_sample_prob, 'sdcvault.table_sample_prob', -1) -%}
+{%- set src_ldts = sdcvault.replace_standard(src_ldts, 'sdcvault.ldts_alias', 'last_updated') -%}
+{%- set src_rsrc = sdcvault.replace_standard(src_rsrc, 'sdcvault.rsrc_alias', 'dv_source') -%}
+{%- set high_water_mark_bool = sdcvault.replace_standard(high_water_mark_bool, 'sdcvault.high_water_mark_bool', true) -%}
+{%- set limit_sources_num = sdcvault.replace_standard(limit_sources_num, 'sdcvault.limit_sources_num', -1) -%}
+{%- set table_sample_prob = sdcvault.replace_standard(table_sample_prob, 'sdcvault.table_sample_prob', -1) -%}
 
 {%- if limit_sources_num != -1 -%}
     {%- set source_models = source_models[:limit_sources_num] -%}
@@ -25,38 +25,38 @@ with
 
 {#- Get available HKs from stage -#}
 src_union as (
-    {% for source_model in source_models -%}
-    select
-        {{ datavault4dbt.print_list(source_cols) }}
+    {% for source_model in source_models %}
+    select {{ datavault4dbt.print_list(source_cols) }}
     from {{ ref(source_model) }}
 
     {%- if table_sample_prob != -1 %}
     tablesample ({{ table_sample_prob }})
-    {% endif -%}
-
-    {%- if not loop.last %}
+    {% endif %}
+    {% if not loop.last %}
     union all
-    {% endif -%}
-    {% endfor -%}
+    {%- endif %}
+    {% endfor %}
 ),
 
 
 {# Distinct HKs -#}
 src_union_first as (
-    select
-        {{ datavault4dbt.print_list(source_cols) }}
+
+    select {{ datavault4dbt.print_list(source_cols) }}
     from src_union
     qualify row_number() over (partition by {{ parent_hash_key }} order by {{ src_ldts }}, {{ src_rsrc }}) = 1
+
 ),
 
 
 {% if is_incremental() -%}
 {# Get latest record per key from esat in incremental runs -#}
 esat_latest as (
-    select
-        {{ datavault4dbt.print_list(source_cols) }}
+
+    select {{ datavault4dbt.print_list(source_cols) }}
     from {{ this }} sat
     qualify row_number() over (partition by {{ parent_hash_key }} order by {{ src_ldts }}) = 1
+
 ),
 {%- endif %}
 
@@ -65,8 +65,7 @@ esat_latest as (
 insert_rows as (
 
     {# Insert records from hub with is_deleted=false, if its not yet available -#}
-    select
-        {{ datavault4dbt.print_list(source_cols) }},
+    select {{ datavault4dbt.print_list(source_cols) }},
         {{ src_ldts }} as start_date,
         to_timestamp({{ end_of_time }}) AS end_date,    
         false as is_deleted 
@@ -112,16 +111,16 @@ insert_rows as (
         {{ src_rsrc }},
         stg.{{ src_ldts }} as start_date,
         to_timestamp({{ end_of_time }}) as end_date,
-        false as is_deleted 
+        false as is_deleted
+
     from src_union_first stg
     left join esat_latest esat
         on {{ datavault4dbt.multikey(parent_hash_key, prefix=['stg', 'esat'], condition='=') }}
     where esat.parent_hash_key is not null
         and esat.is_deleted
-
-{%- endif %}
+{% endif %}
 )
 
-select * from insert_rows
 
+select * from insert_rows
 {%- endmacro %}
