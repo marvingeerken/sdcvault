f48a0d98a02d5ce527bc7a68139c371e56954852
Marvin Geerken <134588432+marvingeerken@users.noreply.github.com>
2024-05-31 13:07:45 +0000

    natural key varchar len

--- macros/hashing/natural_key.sql
+++ macros/hashing/natural_key.sql
@@ -1,11 +1,11 @@
-{%- macro natural_key(field_list) -%}
+{%- macro natural_key(field_list, varchar_length=120) -%}
 
-    {{ adapter.dispatch('natural_key', 'sdcvault')(field_list=field_list) -}}
+    {{ adapter.dispatch('natural_key', 'sdcvault')(field_list=field_lis, varchar_length=varchar_length) -}}
 
 {%- endmacro -%}
 
 
-{%- macro default__natural_key(field_list, varchar_length=120) -%}
+{%- macro default__natural_key(field_list, varchar_length) -%}
 
 {%- set all_null = [] -%}
 {%- for field in field_list -%}
